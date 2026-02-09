e66d77855b9b3771a4a594362700fd33983fd6ec
Satwik2529 <satwik2529@gmail.com>
2026-02-09 21:22:30 +0530

    update1

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,4 +1,5 @@
 const User = require('../models/User');
+const CustomerUser = require('../models/CustomerUser');
 const { generateToken } = require('../middleware/auth');
 const { validationResult } = require('express-validator');
 
